2e89f7ce3473e1af5cd52725dac15bb1c398d8ba
Jeremy Peters <jeremypeters3.14159@gmail.com>
2017-03-27 23:29:59 -0300

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,14 +1,35 @@
 //Where the AI will play it's turn
 import java.util.Random;
-public class AI extends UIBoard{
+public class AI extends User
+{
 	
 	Random r = new Random();
-	private int num1;
-	private int num2;
+	private int guessX;
+	private int guessY;
 	
-	//Constructor
-	public AI(){
-		int num1 = r.nextInt(getWidth()+1);
-      		int num2 = r.nextInt(getLength()+1);
+	//Constructor sets guesses to random numbers
+	public AI()
+	{
+		guessX = r.nextInt(getWidth()+1);
+      		guessY = r.nextInt(getLength()+1);
 	}
+	
+	public int getGuessX()
+	{
+		return guessX;
+	}
+	public int getGuessY()
+	{
+		return guessY;
+	}
+	public void setGuessX(int guessX)
+	{
+		this.guessX = guessX;
+	}
+	public void setGuessY(int guessY)
+	{
+		this.guessY = guessY;
+	}
+	
+	//TODO:  methods to attack a square and set up ships.
 }
